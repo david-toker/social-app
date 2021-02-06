5c3f7472effaf638dcf02f55d3da5f49ec73302e
David Toker <david.toker@yandex.ru>
2021-02-06 11:57:43 +0200

    create resolvers

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers')
 
-const typeDefs = gql`
-  type Post{
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (error) {
-        throw new Error(error);
-      }
-    }
-  }
-}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-// 8odgNcDjWUN97VG3
-// mongodb+srv://david:<password>@cluster0.w5na0.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5na0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
