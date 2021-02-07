b4507fa54f7a9b367fe6516758de50c8f7721b7e
David Toker <david.toker@yandex.ru>
2021-02-07 18:15:16 +0200

    login user

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,17 +1,58 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
-
+const { validateRegisterInput, validateLoginInput } = require('../../util/validators');
 const User = require('../../models/user');
+
+function generateToken(user) {
+  return jwt.sign({
+    id: user.id,
+    email: user.email,
+    username: user.username
+  }, process.env.JWT_KEY, { expiresIn: '1h' });
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }){
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if(!valid){
+        throw new UserInputError('Errors', { errors });
+      }
+      const user  = await User.findOne({ username });
+
+      if(!user){
+        errors.general = 'User not found';
+        throw new UserInputError('User not found', { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if(!match){
+        errors.general = 'Wrong credentials';
+        throw new UserInputError('Wrong credentials', { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      }
+
+    },
     async register(
       _,
       {registerInput: {username, email, password, confirmPassword }},
       context,
       info
     ){
+      // validate user data
+      const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
+      if(!valid){
+        throw new UserInputError('Errors', { errors });
+      }
       // make sure user doesnt exist
       const user = await User.findOne({ username });
       if(user) {
@@ -33,11 +74,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign({
-        id: res.id,
-        email: res.email,
-        username: res.username
-      }, process.env.JWT_KEY, { expiresIn: '1h' });
+      const token = generateToken(res);
 
       return {
         ...res._doc,
