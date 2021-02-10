a49a278c3d985a7875b699fec96544cf77d8b999
David Toker <david.toker@yandex.ru>
2021-02-10 19:36:53 +0200

    create and delete comments

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,9 +1,22 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
-  type Post{
+  type Post {
     id: ID!
     body: String!
+    createdAt: String!
+    username: String!
+    comments: [Comment]!
+    likes: [Like]!
+  }
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+  type Like {
+    id: ID!
     createdAt: String!
     username: String!
   }
@@ -29,5 +42,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
