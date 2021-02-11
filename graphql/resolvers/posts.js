6eb955b498adc3ba47bf466f97548250602ad7fa
David Toker <david.toker@yandex.ru>
2021-02-11 18:33:15 +0200

    like post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/post');
 const checkAuth = require('../../util/check-auth');
 const chekAuth = require('../../util/check-auth');
@@ -54,6 +54,28 @@ module.exports = {
         }
       } catch (error) {
         throw new Error(error);
+      }
+    },
+    async likePost(_, { postId }, context){
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if(post){
+        if(post.likes.find(like => like.username === username)){
+          // Post already likes, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          })
+        }
+
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError('Post not found')
       }
     }
   }
