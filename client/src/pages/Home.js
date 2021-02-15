e6d0b11cc4e7bce5084167a96a441535e2cfc220
David Toker <david.toker@yandex.ru>
2021-02-15 03:18:14 +0200

    postcard

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+
+import { Grid } from 'semantic-ui-react'
+import PostCard from '../components/PostCard';
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default function Home() {
+  // const { loading,  data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+  const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
+  console.log(data);
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  )
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts...</h1>
+        ) : (
+          data.getPosts &&
+          data.getPosts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 }
+
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers')
+const resolvers = require('./graphql/resolvers');
+
+const { DB_PASSWORD, DB_NAME, DB_USER } = require('./config');
 
 const pubsub = new PubSub();
 
@@ -13,13 +15,28 @@ const server = new ApolloServer({
 });
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5na0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+// mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5na0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//   console.log('MongoDB connected')
+//   return server.listen({ port: process.env.PORT || 5000});
+// })
+// .then(res => {
+//   console.log(`Server running at port ${res.url}`)
+// })
+// .catch(error => {
+//   console.log(error)
+// });
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.w5na0.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => {
   console.log('MongoDB connected')
-  return server.listen({ port: process.env.PORT || 5000});
+  return server.listen({ port: 5000});
 })
 .then(res => {
   console.log(`Server running at port ${res.url}`)
